61eb3d91f7d5e37710750cf7dbbde68f1cdd43aa
jeanpokou <pokoujeanmarc@gmail.com>
2017-09-24 22:45:56 -0300

    added appbar and  tabs

--- app/src/main/java/com/slack/jeanpokou/mdcodelab/MainActivity.kt
+++ app/src/main/java/com/slack/jeanpokou/mdcodelab/MainActivity.kt
@@ -2,11 +2,18 @@ package com.slack.jeanpokou.mdcodelab
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        with(tabs) {
+            addTab(tabs.newTab().setText("Tab 1"))
+            addTab(tabs.newTab().setText("Tab 2"))
+            addTab(tabs.newTab().setText("Tab 3"))
+        }
     }
 }
