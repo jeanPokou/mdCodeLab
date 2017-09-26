d177b6e98b74e56be1d9bb169aa0cb2517f44a1e
jeanpokou <pokoujeanmarc@gmail.com>
2017-09-25 23:50:03 -0300

    added Fragment and View Pager

--- app/src/main/java/com/slack/jeanpokou/mdcodelab/MainActivity.kt
+++ app/src/main/java/com/slack/jeanpokou/mdcodelab/MainActivity.kt
@@ -2,7 +2,12 @@ package com.slack.jeanpokou.mdcodelab
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.view.ViewPager
+import android.support.v4.app.FragmentPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.support.v4.viewPager
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,10 +15,65 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Adding Toolbar to Main screen
+        setSupportActionBar(toolbar)
+        // Setting View Pager for each Tabs
+        setupViewPager(viewpager)
+
+        // Adding Tabs
         with(tabs) {
             addTab(tabs.newTab().setText("Tab 1"))
             addTab(tabs.newTab().setText("Tab 2"))
             addTab(tabs.newTab().setText("Tab 3"))
+            setupViewPager(viewpager)
         }
+
+
+
+
     }
+
+
+
+    // Add Fragments to Tabs
+    private fun setupViewPager(viewPager: ViewPager) {
+        val adapter = Adapter(supportFragmentManager).apply {
+            addFragment(ListContentFragment(), "List")
+            addFragment(TileContentFragment(), "Tile")
+            addFragment(TileContentFragment(), "Card")
+        }
+        viewPager.adapter = adapter
+
+    }
+
+    class Adapter(fm: FragmentManager?) : FragmentPagerAdapter(fm) {
+
+        private val mFragmentList = ArrayList<Fragment>()
+        private val mFragmentTitleList = ArrayList<String>()
+
+
+        fun addFragment(fragment: Fragment, title: String) {
+            mFragmentList.add(fragment)
+            mFragmentTitleList.add(title)
+
+        }
+
+        /**
+         * Return the Fragment associated with a specified position.
+         */
+        override fun getItem(position: Int): Fragment {
+            return mFragmentList[position]
+        }
+
+        /**
+         * Return the number of views available.
+         */
+        override fun getCount(): Int {
+            return mFragmentList.size
+        }
+
+
+    }
+
+
 }
